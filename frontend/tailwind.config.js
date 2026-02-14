5d62d0d210668ff01a5099e488be639e9c1661e8
Viraj Induruwa <virajinduruwa2@gmail.com>
2026-02-14 22:22:19 +0530

    fixed the frontend issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,12 +8,12 @@ export default {
     extend: {
       colors: {
         dark: {
-          900: 'var(--color-dark-900)',
-          800: 'var(--color-dark-800)',
-          700: 'var(--color-dark-700)',
-          600: 'var(--color-dark-600)',
-          500: 'var(--color-dark-500)',
-          400: 'var(--color-dark-400)',
+          900: 'rgb(var(--color-dark-900) / <alpha-value>)',
+          800: 'rgb(var(--color-dark-800) / <alpha-value>)',
+          700: 'rgb(var(--color-dark-700) / <alpha-value>)',
+          600: 'rgb(var(--color-dark-600) / <alpha-value>)',
+          500: 'rgb(var(--color-dark-500) / <alpha-value>)',
+          400: 'rgb(var(--color-dark-400) / <alpha-value>)',
         },
         accent: {
           DEFAULT: '#3b82f6',
